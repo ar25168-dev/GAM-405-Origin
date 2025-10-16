0070dd11a80555bfa6ea10c1dfaad875e9c2117a
Anthony Rojas <ar25168@my.acm.ac.uk>
2025-10-16 12:08:43 +0100

    Added a player
    Need to code in camera control at home

---
+++ GAM-405 Maze/Assets/Input/CursorLock.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     Cursor.lockState = CursorLockMode.Locked;   
+    }
+}
